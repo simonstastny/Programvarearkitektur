ae433216613d92ba0fcb80fb6dc7c3ed65776233
Simon Stastny <stastny.simon@gmail.com>
2013-03-01 10:14:54 +0100

    project init

---
+++ StrongDonut-desktop/src/no/ntnu/swa/a13/Main.java
@@ -0,0 +1,16 @@
+package no.ntnu.swa.a13;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "StrongDonut";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new MyGdxGame(), cfg);
+	}
+}
