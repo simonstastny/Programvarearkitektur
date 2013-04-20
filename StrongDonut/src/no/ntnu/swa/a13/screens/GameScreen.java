95b450d5a968d84f118788984712766c3a2dbe93
Sindre N. <nsindre@gmail.com>
2013-04-20 18:04:40 +0200

    Big animated catapult!
    Click to try the awesome catapult, something odd happened with the
    coloring so it remains black for now. This can of course be changed if
    needed.

--- StrongDonut/src/no/ntnu/swa/a13/screens/GameScreen.java
+++ StrongDonut/src/no/ntnu/swa/a13/screens/GameScreen.java
@@ -15,6 +15,7 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -47,13 +48,22 @@ public class GameScreen implements Screen {
 	private OrthographicCamera camera;
 
 	// the SpriteBatch takes care of rendering images
-//	private SpriteBatch batch;
+	private SpriteBatch batch;
 //	private PolygonSpriteBatch polygonBatch;
 	private ShapeRenderer renderer;
 
-	// Menu elements need textures
-//	private Texture newGameTex;
-//	private TextureRegion sketchRegion;
+	//Catapult textures
+	private Texture catapultTex;
+	private TextureRegion[] catapultRegion;
+	private Animation catapultAni;
+	private TextureRegion currentCataFrame;
+	private float animationTimer;
+	//numbers needed to get the right size catapults
+	private int catapultHeightPx = 273;
+	private int catapultWidthPx = 448;
+	private int catapultWidthIncrement = 450;
+	private int catapultMap1stRow = 95;
+	private int catapultMap2ndRow = 545;
 	
 	private Landscape landscape;
 	private Vector2[] landVertices;
@@ -65,16 +75,33 @@ public class GameScreen implements Screen {
 		game = gameRef;
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, MyGdxGame.w, MyGdxGame.h);
-//		batch = new SpriteBatch();
-//		polygonBatch = new PolygonSpriteBatch();
+		batch = new SpriteBatch();
+
 		renderer = new ShapeRenderer();
 		renderer.setTransformMatrix(MyGdxGame.scalingMatrix);
-//		batch.setTransformMatrix(MyGdxGame.scalingMatrix);
+		batch.setTransformMatrix(MyGdxGame.scalingMatrix);
 
-//		newGameTex = new Texture(Gdx.files.internal("data/help_res/gamesketch_1.png"));
-//		newGameTex.setFilter(TextureFilter.Linear, TextureFilter.Linear);// not sure if filter is needed
-//		sketchRegion = new TextureRegion(newGameTex, 0, 0, WIDTH, 512);
+		catapultTex = new Texture(Gdx.files.internal("data/mangonel.png"));
+		catapultTex.setFilter(TextureFilter.Linear, TextureFilter.Linear);// not sure if filter is needed
+		//The individual catapult images are 448px*448px There are 2 px of padding around each image
+		//see .txt file for detailed position of each image
+		//IN ADDITION!: 95 empty pixels above and 80 empty pixels below each image
+		catapultRegion = new TextureRegion[18];
+		for (int i = 0; i < 9; i++){
+			catapultRegion[i] = new TextureRegion(catapultTex, 2+i*catapultWidthIncrement, catapultMap1stRow, catapultWidthPx, catapultHeightPx);
+		}
+		for (int i = 0; i < 9; i++){
+			catapultRegion[i+9] = new TextureRegion(catapultTex, 2+i*catapultWidthIncrement, catapultMap2ndRow, catapultWidthPx, catapultHeightPx);
+		}
+		catapultAni = new Animation(0.05f,catapultRegion);
+		animationTimer = 0;
+		currentCataFrame = catapultAni.getKeyFrame(animationTimer);
+		
+		
+//		p1Region = new TextureRegion(catapultTex, 2, catapultMap1stRow, catapultWidthPx, catapultHeightPx);
+//		p2Region = new TextureRegion(p1Region);
 
+		//below: Vector holds x and y components of gravity
 		world = new World(new Vector2(0, -100), false); // FIXME what does this
 														// mean
 
@@ -111,17 +138,19 @@ public class GameScreen implements Screen {
 		bd.position.y = MyGdxGame.hR/2;
 		bd.type = BodyType.DynamicBody;
 		ball = world.createBody(bd);
-		CircleShape ballShape = new CircleShape();
-		ballShape.setRadius(15);
-		ball.createFixture(ballShape, 100f);
-		ballShape.dispose();
+//		CircleShape ballShape = new CircleShape();
+//		ballShape.setRadius(15);
+//		ball.createFixture(ballShape, 100f);
+//		ballShape.dispose();
 		  
 	}
 
 	@Override
 	public void render(float delta) {
 		
+		Gdx.gl.glClearColor(1f, 1f, 1f, 0f);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
 
 		renderer.setProjectionMatrix(camera.combined);
 		
@@ -132,24 +161,36 @@ public class GameScreen implements Screen {
 		}
 		
 		renderer.end();
-		renderer.begin(ShapeType.Circle);
-		renderer.setColor(Color.BLUE);
-		renderer.circle(ball.getPosition().x, ball.getPosition().y, 15);
-		renderer.end();
+//		renderer.begin(ShapeType.Circle);
+//		renderer.setColor(Color.BLUE);
+//		renderer.circle(ball.getPosition().x, ball.getPosition().y, 15);
+//		renderer.end();
+		
+		batch.setProjectionMatrix(camera.combined);
+		
+		batch.begin();
+		batch.setColor(Color.BLUE);
+		batch.draw(currentCataFrame, MyGdxGame.wR/6, landscape.getSlices().get((int)MyGdxGame.wR/6).height);
+		batch.end();
 
 		//FIXME wifikundace
 		if(Gdx.input.isTouched()){
-			landscape.deform(new Vector2(Gdx.input.getX(),HEIGHT-Gdx.input.getY()), 50);
-			ChainShape landShape = new ChainShape();
-			int it = 0;
-			for (Rectangle slice : landscape.getSlices()){
-				landVertices[it] = new Vector2(slice.x,slice.height);
-				it++;
-			};
-			landShape.createChain(landVertices);		
-			landBody.createFixture(landShape , 1f); //static body density does not matter
-			landShape.dispose();
-			landBody.destroyFixture(landBody.getFixtureList().get(1)); //the fixture at 0 is the floor
+			
+			animationTimer += delta;
+			currentCataFrame = catapultAni.getKeyFrame(animationTimer, true);
+			
+			
+//			landscape.deform(new Vector2(Gdx.input.getX(),HEIGHT-Gdx.input.getY()), 50);
+//			ChainShape landShape = new ChainShape();
+//			int it = 0;
+//			for (Rectangle slice : landscape.getSlices()){
+//				landVertices[it] = new Vector2(slice.x,slice.height);
+//				it++;
+//			};
+//			landShape.createChain(landVertices);		
+//			landBody.createFixture(landShape , 1f); //static body density does not matter
+//			landShape.dispose();
+//			landBody.destroyFixture(landBody.getFixtureList().get(1)); //the fixture at 0 is the floor
 		}
 		
 		world.step(delta, 6, 2);//put a fraction in place of delta for a fixed framerate
@@ -188,7 +229,8 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void dispose() {
-//		batch.dispose();
+		batch.dispose();
+		renderer.dispose();
 		// TODO Auto-generated method stub
 
 	}
